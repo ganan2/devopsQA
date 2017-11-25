58deb5bc6449521cceb4445a476bbbe30b437558
ganan2 <ganan2@uis.edu>
2017-11-25 01:55:26 -0600

    Spring starter with Mongo

--- src/main/java/com/product/rest/repos/UserMongoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.product.rest.repos;
-
-import com.product.rest.pojos.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserMongoRepository extends MongoRepository<User, Integer>{
-    User findByName(String name);
-}
